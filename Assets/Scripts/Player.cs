9675b9bdedb2657efff2056393e0520fbecafd49
Luiz Lopes <luizlopesdev@gmail.com>
2021-05-09 18:39:40 -0300

    player resizing and rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,28 @@ public class Player : MonoBehaviour
     [SerializeField]
     float jumpCooldownTime = 0.5f;
 
+
+    [SerializeField]
+    float scaleSpeed = 5f;
+
+	[SerializeField]
+    float scaleFactor = 1f;
+
+	[SerializeField]
+    float minScale = 0.50f;
+
+	[SerializeField]
+    float maxScale = 2.50f;
+
+    [SerializeField]
+    bool rotate = true;
+
+    [SerializeField]
+    float rotateDuration = 1f;
+
+    Vector2 lastTouchPosition = Vector2.zero;
+
+
     float lastGroundedTime = 0f;
 
     bool grounded;
@@ -26,12 +48,19 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && CanJump()) {
-            Jump();
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x < Screen.width / 2) {
+				Scale(Input.mousePosition);
+			}
+
+            if (Input.mousePosition.x > Screen.width / 2 && CanJump()) {
+                Jump();
+			}
         }
 
-        if (!grounded) {
-            SmoothFallMotion(smoothFallMultiplier);
+        if (body.velocity.y != 0f) {
+            SmoothFallMotion();
         }
     }
 
@@ -42,13 +71,55 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        body.velocity = Vector2.up * jumpForce;
+        body.velocity = Vector2.up * jumpForce * scaleFactor;
         lastGroundedTime = Time.time;
+
+        if (rotate) {
+            StartCoroutine(Rotate(new Vector3(0f, 0f, -90f)));
+        }
     }
 
-    void SmoothFallMotion(float multiplier)
+    void SmoothFallMotion()
     {
-        body.velocity += Vector2.up * Physics2D.gravity.y * (multiplier - 1) * Time.deltaTime;
+        body.velocity += Physics2D.gravity.y * Vector2.up * scaleFactor * smoothFallMultiplier * Time.deltaTime;
+    }
+
+    void Scale(Vector2 currentTouchPosition)
+	{
+		if (currentTouchPosition.y > lastTouchPosition.y) {
+			scaleFactor += scaleSpeed * Time.deltaTime;
+		} else if (currentTouchPosition.y < lastTouchPosition.y) {
+			scaleFactor -= scaleSpeed * Time.deltaTime;
+		} else {
+			scaleFactor = transform.localScale.x;
+		}
+		scaleFactor = Mathf.Clamp(scaleFactor, minScale, maxScale);
+		transform.localScale = new Vector3(scaleFactor, scaleFactor);
+
+		lastTouchPosition = currentTouchPosition;
+	}
+
+    bool rotating;
+    IEnumerator Rotate(Vector3 angle)
+    {
+        if (rotating) {
+            yield return null;
+        }
+
+        rotating = true;
+
+        var target = transform.eulerAngles + angle;
+        var current = transform.eulerAngles;
+
+        float counter = 0;
+        while (counter < rotateDuration)
+        {
+            counter += Time.deltaTime;
+            transform.eulerAngles = Vector3.Lerp(current, target, counter / rotateDuration);
+            yield return null;
+        }
+
+        rotating = false;
     }
 
 	void OnCollisionStay2D(Collision2D collider)
